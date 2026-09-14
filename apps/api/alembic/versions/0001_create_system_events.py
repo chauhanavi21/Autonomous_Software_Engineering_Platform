bfa9f3bc498f314ada2745e0caa909c3eef932f1
avi212003 <chauhanavi212003@gmail.com>
2026-09-13 23:26:25 -0400

    I cleaned up the initial database migration

--- apps/api/alembic/versions/0001_create_system_events.py
+++ apps/api/alembic/versions/0001_create_system_events.py
@@ -6,9 +6,8 @@ Revises:
 from collections.abc import Sequence
 
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
 from alembic import op
+from sqlalchemy.dialects import postgresql
 
 revision: str = "0001"
 down_revision: str | None = None
